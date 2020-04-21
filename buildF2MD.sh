9cbbe00737a4f069828048942b1904900537b17c
Joseph KAMEL <josephekamel@gmail.com>
2020-04-21 21:46:42 +0200

    fixed build scripts

--- buildF2MD.sh
+++ buildF2MD.sh
@@ -14,16 +14,16 @@ export INET_NED_PATH="${INET_ROOT}/src:${INET_ROOT}/tutorials:${INET_ROOT}/showc
 export INET_OMNETPP_OPTIONS="-n ${INET_NED_PATH} --image-path=${INET_ROOT}/images"
 export INET_GDB_OPTIONS="-quiet -ex run --args"
 export INET_VALGRIND_OPTIONS="-v --tool=memcheck --leak-check=yes --show-reachable=no --leak-resolution=high --num-callers=40 --freelist-vol=4000000"
-make makefiles && make -j$(nproc) && make -j$(nproc) WITH_OSGEARTH=no MODE=debug
+make makefiles && make -j$(nproc) WITH_OSGEARTH=no MODE=debug && make -j$(nproc) MODE=release all  
 
 # Build veins
 cd ${F2MD_DIR}/veins-f2md
-./configure && make -j$(nproc)
+./configure && make -j$(nproc) MODE=release all
 
 # Build veins_inet3
 cd ${F2MD_DIR}/veins-f2md/subprojects/veins_inet3
-./configure && make -j$(nproc)
+./configure && make -j$(nproc) MODE=release all
 
 # Build simulte
 cd ${F2MD_DIR}/simulte-f2md
-make makefiles && make -j$(nproc)
+make makefiles && make -j$(nproc) MODE=release all
